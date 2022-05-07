31c1e2dadad3fe51b9f7eba9d86cd6075672a6de
pepamartinka <91247604+pepamartinka@users.noreply.github.com>
2022-05-07 11:31:28 +0200

    Hotovo-server

--- main.py
+++ main.py
@@ -1,15 +1,73 @@
+radio.set_group(202)
+
+#stisknutím loga povoluju/zakazuju hlasování
+#při stisku tlacitek AB najednou zobrazím výsledky
+#při stisku tlacitka A vynuluju výsledky
+
 pocetA=0
 pocetB=0
 pocetC=0
 pocetD=0
 
-def on_received_value(name, value):
-    global pocetA, pocetB, pocetC, pocetD
-    if name == "vote" and value == 1:
-        pocetA +=1
-        basic.show_string("A")
+enable=1
+def on_logo_event_pressed():
+    global enable
+    if enable==1:
+        radio.send_value("zapnuto", 1)
+        enable=0
+    else:
+        radio.send_value("vypnuto", 0)
+        enable=1
+    if enable==1:
+            basic.show_icon(IconNames.NO)
+    else: basic.show_icon(IconNames.YES)
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_event_pressed)
 
-    if name == "vote" and value == 2:
+
+vysledek=0
+if enable==1:
+    def on_received_value(name, value):
+        global vysledek
+        if name == "vote" and value == 1:
+            vysledek=1
+        if name == "vote" and value == 2:
+            vysledek=2
+        if name == "vote" and value == 3:
+            vysledek=3
+        if name == "vote" and value == 4:
+            vysledek=4
+    radio.on_received_value(on_received_value)
+else:
+    if vysledek==1:
+        pocetA+=1
+    elif vysledek==2:
         pocetB+=1
-radio.on_received_value(on_received_value)
+    elif vysledek==3:
+        pocetC+=1
+    elif vysledek==4:
+        pocetD+=1
+    radio.send_value("uspesne", 5)
+
+def on_button_pressed_ab():
+    global pocetA,pocetB,pocetC,pocetD
+    basic.show_string(" A=")
+    basic.show_number(pocetA)
+    basic.show_string(" B=")
+    basic.show_number(pocetB)
+    basic.show_string(" C=")
+    basic.show_number(pocetC)
+    basic.show_string(" D=")
+    basic.show_number(pocetD)
+    basic.clear_screen()
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_a():
+    global pocetA,pocetB,pocetC,pocetD
+    pocetA=0
+    pocetB=0
+    pocetC=0
+    pocetD=0
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+
 
--- main.ts
+++ main.ts
@@ -1,16 +1,80 @@
+radio.setGroup(202)
+// stisknutím loga povoluju/zakazuju hlasování
+// při stisku tlacitek AB najednou zobrazím výsledky
+// při stisku tlacitka A vynuluju výsledky
 let pocetA = 0
 let pocetB = 0
 let pocetC = 0
 let pocetD = 0
-radio.onReceivedValue(function on_received_value(name: string, value: number) {
+let enable = 1
+input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_event_pressed() {
     
-    if (name == "vote" && value == 1) {
+    if (enable == 1) {
+        radio.sendValue("zapnuto", 1)
+        enable = 0
+    } else {
+        radio.sendValue("vypnuto", 0)
+        enable = 1
+    }
+    
+    if (enable == 1) {
+        basic.showIcon(IconNames.No)
+    } else {
+        basic.showIcon(IconNames.Yes)
+    }
+    
+})
+let vysledek = 0
+if (enable == 1) {
+    radio.onReceivedValue(function on_received_value(name: string, value: number) {
+        
+        if (name == "vote" && value == 1) {
+            vysledek = 1
+        }
+        
+        if (name == "vote" && value == 2) {
+            vysledek = 2
+        }
+        
+        if (name == "vote" && value == 3) {
+            vysledek = 3
+        }
+        
+        if (name == "vote" && value == 4) {
+            vysledek = 4
+        }
+        
+    })
+} else {
+    if (vysledek == 1) {
         pocetA += 1
-        basic.showString("A")
-    }
-    
-    if (name == "vote" && value == 2) {
+    } else if (vysledek == 2) {
         pocetB += 1
+    } else if (vysledek == 3) {
+        pocetC += 1
+    } else if (vysledek == 4) {
+        pocetD += 1
     }
     
+    radio.sendValue("uspesne", 5)
+}
+
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    
+    basic.showString(" A=")
+    basic.showNumber(pocetA)
+    basic.showString(" B=")
+    basic.showNumber(pocetB)
+    basic.showString(" C=")
+    basic.showNumber(pocetC)
+    basic.showString(" D=")
+    basic.showNumber(pocetD)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    pocetA = 0
+    pocetB = 0
+    pocetC = 0
+    pocetD = 0
 })
